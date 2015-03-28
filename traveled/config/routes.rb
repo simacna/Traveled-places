7c56db3842cb65fa22f2b250c1003de48792812c
simacna <simacna@gmail.com>
2015-03-28 16:34:19 -0400

    added new controller/view for user to login

--- traveled/config/routes.rb
+++ traveled/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  
+
+  root 'home#index'
 
   # root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
