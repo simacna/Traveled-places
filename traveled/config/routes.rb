41af6e0e2c3832b2a08f0de453e6a41a71e9c167
simacna <simacna@gmail.com>
2015-03-31 14:24:22 -0400

    creating new controller

--- traveled/config/routes.rb
+++ traveled/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get 'callback' => 'users#oauth_callback'
+
+
   # root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
