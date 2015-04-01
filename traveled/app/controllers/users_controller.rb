fe8d18ecda5941fcb19b9f8bce57acf959ba49d2
simacna <simacna@gmail.com>
2015-04-01 17:56:57 -0400

    adding comments to code to make more clear

--- traveled/app/controllers/users_controller.rb
+++ traveled/app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 
 class UsersController < ApplicationController
 
-	
+	# This will redirect to get 'callback' => 'users#oauth_callback'
   def oauth
     redirect_to Instagram.authorize_url(:redirect_uri => "http://localhost:3000/callback")
   end
 
   def oauth_callback
-  	
+  	#This is the second-call back providing us with access_token
     response = Instagram.get_access_token(params[:code], :redirect_uri => "http://localhost:3000/callback")
     session[:access_token] = response.access_token
 
-	
+	  binding.pry
 	redirect_to '/map'
   end
 
